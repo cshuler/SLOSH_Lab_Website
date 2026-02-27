38791ac212d4032120a17ceb1c923cf7d5bdb671
mewong21 <mwong20@hawaii.edu>
2026-02-27 11:37:09 -1000

    Fixed css

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,8 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("css");
+
+  return {
+    pathPrefix: "/SLOSH_Lab_Website/",
+  };
 };
